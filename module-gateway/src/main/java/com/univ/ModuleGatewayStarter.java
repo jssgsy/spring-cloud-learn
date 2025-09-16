8cd46b84c16f5851c86c22f356cc40ec50b73f3f
univ <fcsnwu@163.com>
2025-09-16 13:57:26 +0800

    spring cloud使用gateway最基础示例

---
+++ module-gateway/src/main/java/com/univ/ModuleGatewayStarter.java
@@ -0,0 +1,17 @@
+package com.univ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author univ
+ * date 2025/9/12
+ */
+@SpringBootApplication
+public class ModuleGatewayStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleGatewayStarter.class);
+    }
+
+}
